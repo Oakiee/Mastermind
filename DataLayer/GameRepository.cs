0fe17f0b7ca63f3d7632e3541058ef312aad27e6
Oakie <amandaoyakawa@gmail.com>
2025-06-26 13:45:00 -0400

    Add project files.

---
+++ DataLayer/GameRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    /// <summary>
+    /// Store of all the data of the current and previous games during the session
+    /// </summary>
+    public class GameRepository
+    {
+
+    }
+}
---
+++ Mastermind/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    //TODO: see if there's a better place to put this for conventional console app architecture
+    /// <summary>
+    /// This is the service that handles the running of the game
+    /// </summary>
+    public class GameHandler
+    {
+
+    }
+}
